53dd22f1dc36cc4ae49640e31655db629851a340
Silwernian <makise.silwer@gmail.com>
2023-08-16 08:22:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import functions.functions as fc
 class OCR:
 
     def __init__(self):
-        st.set_page_config(page_title="Python OCR V1.1", layout='wide')
+        st.set_page_config(page_title="Python OCR V1.2", layout='wide')
         self.texto = ""
         self.analisar_texto = False
 
